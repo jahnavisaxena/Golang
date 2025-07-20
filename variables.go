0335748545d44825025243d44492259a7676c9f1
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-21 00:21:21 +0530

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+func main(){
+	//var name string  ="golang"
+	//automatically infers type
+	//var name ="golang"
+
+	var isAdult =true
+
+	var num int = 43
+
+	//shortahand syntax
+	name:= "golang"
+	fmt.Print(name)
+	fmt.Print(isAdult)
+	fmt.Print(num)
+}
