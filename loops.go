493665cbf0874ccb8a56aefd8b551fd29276da34
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-21 00:40:21 +0530

    Create loops.go

---
+++ loops.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//while llo
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for i:=0; i<4 ; i++{
+		fmt.Println(i)
+	}
+
+	//infinite 
+	for{
+		fmt.Print("tanay")
+	}
+}
