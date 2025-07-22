3345380d334548a6b40be405298ab290c2d78039
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2025-07-22 15:26:59 +0530

    Create arr.go

---
+++ Arrays/arr.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	 
+	//var nums [4]int  
+
+	//nums[0] =1
+	//fmt.Println(nums[0])
+
+	//fmt.Println(len(nums))
+	//fmt.Println(nums)
+
+	var vals[4]bool
+	fmt.Println(vals)
+
+	var name[3]string
+	fmt.Print(name)
+
+	nums:= [3]int{1,2,3}
+	fmt.Println(nums)
+
+	//2d arrays
+	num :=[2][2]int{{2,3},{5,6}}
+	fmt.Println(num)
+}
